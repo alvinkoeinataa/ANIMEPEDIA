bf71894277c2583c95f787106886be6b0929e341
Alvin <alvinkoeinata@gmail.com>
2023-11-19 16:24:41 +0700

    update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,15 +8,12 @@ const Navbar = () => {
     <header className="bg-color-accent">
       <div className="flex md:flex-row flex-col gap-2 justify-between md:items-center p-4">
         <div>
-          <Link href={"/"} className="font-bold text-2xl p-2 mr-4 border border-color-dark">
+          <Link href={"/"} className="font-bold text-2xl p-2 mr-2 border border-color-dark">
             ANIMEPEDIA
-          </Link>
-          <Link href={"/"} className="">
-            <button className="text-xl rounded-lg border text-color-primary border-color-primary p-2 bg-color-secondary mr-3">Home</button>
           </Link>
 
           <Link href={"/char"} className="">
-            <button className="text-xl rounded-lg border text-color-primary border-color-primary p-2 bg-color-secondary">Characters</button>
+            <button className="text-lg rounded-lg border text-color-primary border-color-primary p-2 bg-color-secondary">Characters</button>
           </Link>
         </div>
 
